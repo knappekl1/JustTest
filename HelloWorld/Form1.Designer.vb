117150be82216f1156b4c31dccdfa053a2094e4f
Libor Knappek <libor.knappek@gmail.com>
2021-01-04 22:40:10 +0100

    first commit

---
+++ HelloWorld/Form1.Designer.vb
@@ -0,0 +1,40 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Test1
+    Inherits System.Windows.Forms.Form
+
+    'Formulář přepisuje metodu Dispose, aby vyčistil seznam součástí.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Vyžadováno Návrhářem Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'POZNÁMKA: Následující procedura je vyžadována Návrhářem Windows Form
+    'Může být upraveno pomocí Návrháře Windows Form.  
+    'Neupravovat pomocí editoru kódu
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '
+        'Test1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(799, 792)
+        Me.Font = New System.Drawing.Font("Consolas", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Name = "Test1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Hello World"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+End Class
---
+++ HelloWorld/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Obecné informace o sestavení se řídí přes následující 
+' sadu atributů. Změnou hodnot těchto atributů se upraví informace
+' přidružené k sestavení.
+
+' Zkontrolujte hodnoty atributů sestavení.
+
+<Assembly: AssemblyTitle("HelloWorld")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("HelloWorld")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Následující GUID se používá pro ID knihovny typů (typelib), pokud je tento projekt vystavený pro COM.
+<Assembly: Guid("a857f0ae-b039-4455-9e63-8dc2d260e82b")>
+
+' Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+'
+'      Hlavní verze
+'      Podverze
+'      Číslo sestavení
+'      Revize
+'
+' Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
+' pomocí zástupného znaku * takto:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
